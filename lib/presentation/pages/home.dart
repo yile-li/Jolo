f5ef5db50c5fbaec682c224414ba95094d279ea1
yile-li <liyile@umich.edu>
2024-03-21 22:28:23 -0400

    Add greeting text on home page

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -8,12 +8,45 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: appBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _greetingText(),
+        ],
+      ),
+    );
+  }
+
+  Column _greetingText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "Good afternoon, User", //TODO: write API to get username
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontFamily: 'PlayfairDisplay',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          "Welcome back to JOLO.",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+      ],
     );
   }
 
   AppBar appBar() {
     return AppBar(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.black,
       leading: Icon(Icons.abc, size: 50, color: Colors.white),
       actions: [
         GestureDetector(
